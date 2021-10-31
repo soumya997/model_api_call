df132b6d138072bdcd8e0c060e6db249741fb2a4
Soumyadip Sarkar <54326088+soumya997@users.noreply.github.com>
2021-10-31 18:46:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,request
+from flask import jsonify
+import requests
+# from flask_ngrok import run_with_ngrok
+app = Flask(__name__)
+# run_with_ngrok(app)
+url = "http://42fb-35-196-0-30.ngrok.io"
+@app.route("/api",methods=['GET'])
+def home():
+  qs = request.args.get('qs')
+  qs_url = qs.replace(' ', '%20')
+  ans = requests.get(url+ "/api?qs=" + qs_url)
+  return ans
+
+if __name__ == "__main__":
+  app.run()
