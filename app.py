140dbb4a8ef1f25326731411d813a1bdd8fefab4
Soumyadip Sarkar <54326088+soumya997@users.noreply.github.com>
2021-10-31 20:15:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 # from flask_ngrok import run_with_ngrok
 app = Flask(__name__)
 # run_with_ngrok(app)
-url = "http://16cd-34-72-55-62.ngrok.io"
+url = "http://652f-104-199-206-225.ngrok.io"
 @app.route("/api",methods=['GET'])
 def home():
   qs = request.args.get('qs')
